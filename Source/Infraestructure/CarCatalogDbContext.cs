d5f59cba6cb276551d61107c2c08745453a96a79
Venilson1 <Venilson2009@hotmail.com>
2022-10-25 20:56:50 -0300

    feat(car): criacao dbContex

--- Source/Infraestructure/CarCatalogDbContext.cs
+++ Source/Infraestructure/CarCatalogDbContext.cs
@@ -1,8 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using CarCatalogAPI.Source.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarCatalogAPI.Source.Infraestructure
 {
     public class CarCatalogDbContext : DbContext
     {
+        public CarCatalogDbContext(DbContextOptions<CarCatalogDbContext> options) : base(options) { }
+
+        public DbSet<Car> Cars { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
     }
 }
