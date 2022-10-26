021b953a489eed8d2229c8b4ff26bdf5142519fd
Venilson1 <Venilson2009@hotmail.com>
2022-10-25 21:41:39 -0300

    feat(car): injetando dependencia de conexao e interface

--- Program.cs
+++ Program.cs
@@ -1,6 +1,17 @@
+using CarCatalogAPI.Source.Core.Interfaces.Repositories;
+using CarCatalogAPI.Source.Infraestructure;
+using CarCatalogAPI.Source.Infraestructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddEntityFrameworkSqlServer()
+    .AddDbContext<CarCatalogDbContext>(options => 
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+
+builder.Services.AddScoped <ICarRepository, CarRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
