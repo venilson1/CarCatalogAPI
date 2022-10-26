3e9028a4ff668ca5cc13fb92075c5c388ed12ac8
Venilson1 <Venilson2009@hotmail.com>
2022-10-26 10:01:05 -0300

    refactor(exception): romocao da pasta de exceptions

--- Source/Core/Exceptions/CarNotFoundException.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace CarCatalogAPI.Source.Core.Exceptions
-{
-    public class CarNotFoundException : Exception
-    {
-        public CarNotFoundException(string message = "Id não encontrado") : base(message)
-        {
-        }
-    }
-}
